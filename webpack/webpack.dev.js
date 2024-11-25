4fc4c96041017795e7e8d9d4fdcaf037af889eb0
Sergey <sergeykachaliukdev@gmail.com>
2024-11-25 21:01:57 +0300

    setup: react-refresh

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,8 +1,12 @@
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+
 module.exports = {
-  mode: "development",
-  devServer: {
-    hot: true,
-    open: true,
-  },
-  devtool: "cheap-module-source-map", // optimal size and speed. Source map helps us to debug with comparing lines and expressions of compiled code and source
+    mode: "development",
+    devServer: {
+        hot: true,
+        open: true,
+    },
+    // optimal size and speed. Source map helps us to debug with comparing lines and expressions of compiled code and source
+    devtool: "cheap-module-source-map",
+    plugins: [new ReactRefreshWebpackPlugin()],
 };
