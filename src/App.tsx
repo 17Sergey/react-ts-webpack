0e6a22c3c8aca80bd16947448d0815312cbe6d53
Sergey <sergeykachaliukdev@gmail.com>
2024-11-25 20:01:18 +0300

    setup: add eslint, eslint plugins and .eslintrc.js

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,8 @@ export const App = () => {
     <div>
       <h1>React TypeScript Webpack starter template</h1>
       <p>
-        JSX also works, let's insert current data: {new Date().toDateString()}
+        JSX also works, let&apos;s insert current data:{" "}
+        {new Date().toDateString()}
       </p>
       <img src={Car} alt="" width={800} height={500} />
       <img src={ReactIcon} alt="" width={300} height={200} />
